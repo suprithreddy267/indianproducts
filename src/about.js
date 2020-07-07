424498d748af26fd3353545815c431ea918c2cbd
suprithreddy267 <52496136+suprithreddy267@users.noreply.github.com>
2020-07-07 20:33:20 +0530

    pushed

--- src/about.js
+++ src/about.js
@@ -5,13 +5,31 @@ function About(){
 	
 
 	return(
-	<div style={{marginTop:'5em'}}>
+	<div>
 		<div style={{display: "flex",justifyContent: "center",alignItems: "center"}}>
 		<img src={require("./gdp.jpg")} width='50%'/>
 		</div>
 		<br></br>
-		<div>
-		<p style={{display: "flex",justifyContent: "center",color:'ivory',alignItems: "center",fontSize:'30px'}}>The already slowing Indian economy is now faced with COVID-19, a global pandemic which is fast turning into an economic pandemic. The economic impact of the COVID-19 will be a function of the magnitude and speed at which it spreads and duration over which it lasts within India and across the globe.</p>
+		<div style={{display: "flex",justifyContent: "center",color:'#204051',alignItems: "center",fontSize:'3vmax'}}>
+		Atmanirbhar Bharat (transl. self-reliant India) is the vision of the Prime Minister of India Narendra Modi of making India a self-reliant nation. The first mention of this came in the form of the 'Atmanirbhar Bharat Abhiyan' or 'Self-Reliant India Mission' during the announcement of the coronavirus pandemic related economic package on 12 May 2020. This self-reliant policy does not aim to be protectionist in nature and as the Finance Minister clarified, "self-reliant India does not mean cutting off from rest of the world".The law and IT minister, Ravi Shankar Prasad, said that self-reliance does "not mean isolating away from the world. Foreign direct investment is welcome, technology is welcome.self-reliant India... translates to being a bigger and more important part of the global economy."<br></br>
+		PM Modi urges people to use 'Made in India' products<br></br>
+		PM MODI : Please Use Indian Product As Much As You Can<br></br>
+
+		To Convert Atma Nirbhar Bharat Into Reality, PM Modi Now Needs to Wage a War<br></br>
+		GDP(Gross Domestic Product) is the feature that describes the total earning of an economy in a fiscal year. It comprises of the total dollar values of the goods and services produced in a year. 
+		Now, if we use an item made by a foreign company, we basically contribute money to a foreign company, hence to a foreign nation. So, if we use items made in India by Indian companies, we increase the total output of Indian companies and hence the GDP of India. This does contribute positively to the economy.<br></br>
+		The already slowing Indian economy is now faced with COVID-19, a global pandemic which is fast turning into an economic pandemic. The economic impact of the COVID-19 will be a function of the magnitude and speed at which it spreads and duration over which it lasts within India and across the globe.<br></br>
+		Atma Nirbhar executable; small businesses should have easy access to credit: KV Kamath<br></br>
+		NEW DELHI: Reiterating Prime Minister Narendra Modi’s call for an ‘Atma Nirbhar Bharat’ or self-reliant India, ministers and senior government officials on Tuesday exhorted the media and entertainment sector to come up with content conceptualized and executed entirely in the country even as covid-19 challenges persist.<br></br>
+		On the agri-imports front, the biggest item is edible oils — worth about $10 billion (more than 15 mt). This is where there is a need to create “aatma nirbharta”, not by levying high import duties, but by creating a competitive advantage through augmenting productivity and increasing the recovery ratio of oil from oilseeds.<br></br>
+		PM urges tech community to participate in Aatmanirbhar Bharat App Innovation Challenge<br></br>
+		PM launches Aatmanirbhar Bharat Innovation Challenge<br></br>
+		Only swadeshi products in CAPF canteens from June 1 after PM Modi’s push<br></br>
+		<br></br>
+		<br></br>No Chinese products: Govt orders BSNL to use Made in India goods after India-China face-off in Ladakh<br></br>
+		What are the benefits of using Indian products, as an Indian citizen?<br></br><br></br>
+		Using Indian products as an Indian citizen surely benefits our Country directly and indirectly. If we use domestic products the money remains in our country which strengthen our economy and it is also advantageous to the small scale as well as large and medium industries of our country…<br></br>
+
 		</div>
 	</div>)
 }
